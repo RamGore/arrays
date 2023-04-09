38ad44f8a045ae9d1c9486d130efb4379a7d7012
Ram Dnyandev Gore <130224491+prover11@users.noreply.github.com>
2023-04-09 23:03:50 +0530

    Add files via upload

---
+++ character array.c
@@ -0,0 +1,11 @@
+//character array print .
+#include<stdio.h>
+void main()
+{
+    char a[10]={'a','b','c','d','e','f','g','h','i','j'};
+    int i;
+    for(i=0;i<10;i++)
+    {
+        printf("%c ",a[i]);
+    }
+}
