5809e5ce1339b51df18419669370356066be07b9
Ram Dnyandev Gore <130224491+prover11@users.noreply.github.com>
2023-04-09 23:25:11 +0530

    Update user defind character array.c

--- user defind character array.c
+++ user defind character array.c
@@ -1,11 +1,11 @@
 #include<stdio.h>
 void main()
 {
-    char i,a[100];
-    printf("Enter the numbers in array:");
-    for(i=0;i<100;i++)
+    char i,a[10];
+    printf("Enter the ten character in array:");
+    for(i=0;i<10;i++)
     scanf("%c",&a[i]);
-    for(i=0;i<100;i++)
+    for(i=0;i<10;i++)
     {
         printf("%c ",a[i]);
     }
