04e1ab841eebbdc54f24968076820a9e3bf3bcd3
Ram Dnyandev Gore <130224491+prover11@users.noreply.github.com>
2023-04-09 22:59:05 +0530

    Add files via upload

---
+++ array single element acces.c
@@ -0,0 +1,11 @@
+//single element access in array
+#include<stdio.h>
+void main()
+{
+    int a[10]={4,5,6,7,8};
+    printf("%d\n",a[0]);
+    printf("%d\n",a[1]);
+    printf("%d\n",a[2]);
+    printf("%d\n",a[3]);
+    printf("%d\n",a[4]);
+}
